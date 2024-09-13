778be8e7d143f1934e33fd8be0babc4368feef22
Jesús Valeta <jvaleta1995@gmail.com>
2024-09-13 17:53:39 -0500

    Se creo la clase UserDetailServiceImpl

--- src/main/java/com/igrowker/miniproject/auth/service/UserDetailsServiceImpl.java
+++ src/main/java/com/igrowker/miniproject/auth/service/UserDetailsServiceImpl.java
@@ -1,2 +1,17 @@
-package com.igrowker.miniproject.auth.service;public class UserDetailsServiceImpl {
+package com.igrowker.miniproject.auth.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("userDetailsService")
+@Transactional(readOnly = true)
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
