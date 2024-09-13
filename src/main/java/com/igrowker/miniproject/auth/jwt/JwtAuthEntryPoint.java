73d9a6c7d67dffc37840693560882c1baf2b984d
Jesús Valeta <jvaleta1995@gmail.com>
2024-09-13 18:51:58 -0500

    se creo la clase JwtEntryPoint

--- src/main/java/com/igrowker/miniproject/auth/jwt/JwtAuthEntryPoint.java
+++ src/main/java/com/igrowker/miniproject/auth/jwt/JwtAuthEntryPoint.java
@@ -1,2 +1,33 @@
-package com.igrowker.miniproject.auth.jwt;public class JwtAuthEntryPoint {
+package com.igrowker.miniproject.auth.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        final Map<String, Object> error = new HashMap<>();
+        error.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+        error.put("error", "Unauthorized");
+        error.put("message", "Acceso no autorizado al recurso: " + authException.getMessage());
+        error.put("path", request.getServletPath());
+
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), error);
+    }
 }
