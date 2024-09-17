4b8d0a7023b5e6a816e39f4223814b01261769a7
Jesús Valeta <jvaleta1995@gmail.com>
2024-09-17 12:47:44 -0500

    Agregando descripcion de Swagger a los endpoints de AuthController

--- src/main/java/com/igrowker/miniproject/auth/controller/AuthController.java
+++ src/main/java/com/igrowker/miniproject/auth/controller/AuthController.java
@@ -4,6 +4,8 @@ import com.igrowker.miniproject.auth.dto.JwtResponseDto;
 import com.igrowker.miniproject.auth.dto.LoginDto;
 import com.igrowker.miniproject.auth.dto.RegisterDto;
 import com.igrowker.miniproject.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@Tag(name = "Authentication", description = "Authentication API")
 public class AuthController {
     private final AuthService authService;
 
@@ -20,11 +23,13 @@ public class AuthController {
         this.authService = authService;
     }
 
+    @Operation(summary = "Allows the user to log in and obtain the authentication token")
     @PostMapping("/login")
     public ResponseEntity<JwtResponseDto> login(@RequestBody LoginDto loginDto) {
         return ResponseEntity.ok(authService.login(loginDto));
     }
 
+    @Operation(summary = "Allows the user to create an account")
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
         authService.register(registerDto);
