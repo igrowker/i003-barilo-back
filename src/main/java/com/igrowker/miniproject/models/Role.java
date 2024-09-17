d57d3018e43f4ef30e03a65f34a5e0bce27afe26
Jesús Valeta <jvaleta1995@gmail.com>
2024-09-16 21:40:11 -0500

    Se creo la entidad role

---
+++ src/main/java/com/igrowker/miniproject/models/Role.java
@@ -0,0 +1,25 @@
+package com.igrowker.miniproject.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
