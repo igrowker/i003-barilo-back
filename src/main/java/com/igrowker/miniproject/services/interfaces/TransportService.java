a21ea979246a8188b11a870b977c6dcbc489048b
Miguel Angel Chavez <chavez.miguel.angel154@gmail.com>
2024-09-22 22:51:28 -0300

    refactor: renombra las clases para mantener el estándar

--- src/main/java/com/igrowker/miniproject/services/interfaces/TransportService.java
+++ src/main/java/com/igrowker/miniproject/services/interfaces/TransportService.java
@@ -7,7 +7,7 @@ import com.igrowker.miniproject.dtos.TransportDto;
 import com.igrowker.miniproject.dtos.TransportFilterDto;
 import com.igrowker.miniproject.dtos.req.CreateTransportDto;
 
-public interface ITransportService {
+public interface TransportService {
     Page<TransportDto> getAllTransports(TransportFilterDto transportFilterDto, Pageable pageable);
     TransportDto getTransportById(Long id);
     TransportDto createTransport(CreateTransportDto transportDto);
