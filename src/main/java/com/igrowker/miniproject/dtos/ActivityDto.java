c60acb3d56db0dccccecff72fa87f469fce3d4a3
Braulio Jaureguy <55444408+Brau1973@users.noreply.github.com>
2024-09-26 22:49:46 -0300

    Update ActivityDto.java

--- src/main/java/com/igrowker/miniproject/dtos/ActivityDto.java
+++ src/main/java/com/igrowker/miniproject/dtos/ActivityDto.java
@@ -4,13 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class ActivityDto {
     Long id;
     String name;
-    String price;
-    String description; 
+    String description;
+    BigDecimal price;
     String destinationName;
 }
