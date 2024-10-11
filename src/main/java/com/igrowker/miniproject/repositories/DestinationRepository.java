a0a8a1039c534b7af09c505365024b669be7d24e
Jesús Valeta <jvaleta1995@gmail.com>
2024-10-10 22:01:48 -0500

    Obtener destinos por id, name y city

--- src/main/java/com/igrowker/miniproject/repositories/DestinationRepository.java
+++ src/main/java/com/igrowker/miniproject/repositories/DestinationRepository.java
@@ -1,13 +1,17 @@
 package com.igrowker.miniproject.repositories;
 
 import com.igrowker.miniproject.models.Destination;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DestinationRepository extends JpaRepository<Destination, Long> {
 
     Optional<Destination> findByName(String name);
+    Page<Destination> findAllByCity(String city, Pageable pageable);
 }
